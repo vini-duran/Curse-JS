e4aab71914bcee1d0cc0b27644270dc96c1d4731
Vinicius Duran <duranmoura_13@hotmail.com>
2024-06-29 18:36:10 -0400

    JavaScrip.011
    Aula011

---
+++ Aula011/Aula011.js
@@ -0,0 +1,6 @@
+var vel = 78
+console.log(`A velocidade do seu carro é ${vel}Km/h `)
+if (vel > 60) { //É uma condição simples
+    console.log(`Voce ultrapassou o limite permitido. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
